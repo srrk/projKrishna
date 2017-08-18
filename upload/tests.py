d21a1bf13366b8260adc03b2622ba0b6993fcf63
SR Rajesh Kumar <rajeshux@gmail.com>
2017-08-18 15:16:29 +0530

    Unit test for index view added

--- upload/tests.py
+++ upload/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def test_home_page_is_available(self):
+        client = Client()
+        url = reverse('index')
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, 200)
