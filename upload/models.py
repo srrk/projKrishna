14332a0a70e8c6a2eba7005366c4dfac3242f73b
SR Rajesh Kumar <rajeshux@gmail.com>
2017-08-23 11:17:57 +0530

    Participant model created with associated attributes and intial migrations

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Participant(models.Model):
+    reg_number = models.IntegerField(unique=True)
+    name = models.CharField(max_length=256)
+    org_name = models.CharField(max_length=256)
+    # still more to come.
